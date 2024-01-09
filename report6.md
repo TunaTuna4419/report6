6fdca341f4b6aa100c6c190fa1838cec301987dc
Tsuha Shintaro <e235734@eve.u-ryukyu.ac.jp>
2024-01-09 12:52:36 +0900

    gradle added

--- report6.md
+++
@@ -1,3 +0,0 @@
-# 課題レポート6:Java最終課題
-リンク:(https://github.com/naltoma/java_intro2020/blob/master/report/report6_final.md)
-
--- report6/report6.java
+++
@@ -1,7 +0,0 @@
-package report6;
-
-public class report6 {
-    public static void main(String[] args) {
-        System.out.println("Hello World");
-    }
-}
