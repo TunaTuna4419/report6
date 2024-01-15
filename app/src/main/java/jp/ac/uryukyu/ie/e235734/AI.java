8cae3fab2e310b409980b8b5053a226485665630
Tsuha Shintaro <e235734@eve.u-ryukyu.ac.jp>
2024-01-15 11:50:14 +0900

    edited AI.java

--- app/src/main/java/jp/ac/uryukyu/ie/e235734/AI.java
+++ app/src/main/java/jp/ac/uryukyu/ie/e235734/AI.java
@@ -2,12 +2,19 @@ package jp.ac.uryukyu.ie.e235734;
 import java.util.Scanner;
 
 public class AI {
+
     Scanner scanner = new Scanner(System.in);
     Weather weather = new Weather();
     Greeting greeting = new Greeting();
     public void start(){
-        System.out.println("コマンドを入力してください。");
-        String command = scanner.next();
-        System.out.println("コマンドは"+ command + "です。");
+        while(true){
+            System.out.println("コマンドを入力してください。");
+            String command = scanner.next();
+            System.out.println("コマンドは"+ command + "です。");
+            if(command.equals("exit")){
+                System.out.println("終了します。さようなら");
+                break;
+            }
+        }      
     }
 }
